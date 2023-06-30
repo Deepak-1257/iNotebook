fef33a0d8702d0f04acb27e926c2fa2dbb64a2c4
Deepak-1257 <98898432+Deepak-1257@users.noreply.github.com>
2023-06-30 15:22:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const connectToMongo = require('./db');
 const express = require('express')
 var cors = require('cors') 
-
+const corsOptions={
+origin:"https://inotebook-oym3.onrender.com"
+}
 connectToMongo();
 const app = express()
 const port = process.env.PORT || 5000;
-app.use(cors())
+app.use(cors(corsOptions));
 app.use(express.json())
 
 // // // Available Routes
